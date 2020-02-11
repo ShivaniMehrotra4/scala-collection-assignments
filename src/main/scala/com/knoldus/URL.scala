e008a65d66c86117ce28a4b12e4fe130cb7a5347
ShivaniMehrotra4 <shivanimehrotra.sms@gmail.com>
2020-02-11 23:59:29 +0530

    updated URL.scala

--- src/main/scala/com/knoldus/URL.scala
+++ src/main/scala/com/knoldus/URL.scala
@@ -1,43 +1,32 @@
 package com.knoldus
 
 object URL extends App {
-  def apply(protocol:String,domain:String,path:String, params:Map(String,string: String):String = {
+  def apply(protocol:String,domain:String,path:String,params: Map[String, String]): String = {
 
     protocol + "://" + domain + "/" + path + "?" + params
 
   }
-  def unapply(url:String):Option[(String , String, Map[String,String]] = {
-    //val url : String = "https://aws.amazon.com/console/home?state=hash&isauthcode=true&code=112"
-    val mapExample =Map()
-    def checkEvenOdd(arr : Array[String]): Map[String,String] =
-    {
+  def unapply(url:String):Option[(String , String, Map[String,String])] = {
 
-      mapExample + Map(arr(0)->arr(1))
-
-    }
-    val parts = url.split("://")
-    if(parts.length >1)
-      {
-        val partsInner = parts(1).split("com")
-        if(partsInner.length >1)
-          {
-            partsInner(0) = partsInner(0) + "com"
-            val partsInnerInner = partsInner(1).split("?")
-            if(partsInnerInner.length >1)
-              {
-                  val partsInnerInnerInner : Array[String] = partsInnerInner(1).split("[&\\=]")
-                  //Some(parts(0),partsInner(0),partsInnerInner(0),partsInnerInnerInner  )
-
-              }
-            else
-              None
-          }
-        else
-          None
+    @scala.annotation.tailrec
+    def checkEvenOdd(mapFinal: Map[String, String], arr: Array[String]): Map[String, String] = {
+      if (arr.length > 1) {
+        checkEvenOdd(mapFinal ++ Map(arr(0) -> arr(1)), arr.slice(2, arr.length))
       }
-else
-      None
-  }
-
+      else
+        mapFinal
+    }
+    val v1 = url.split("://")
+    if (v1.length > 1) {
+      val v2 = v1(1).split("(?<=com)")
+      val v3 = v2(1).split("[?]")
+      val v4: Array[String] = v3(1).split("[=\\&]")
+      val checkResult = checkEvenOdd(Map.empty[String, String], v4)
+      Some(v1(0),v2(0),v3(0),checkResult)
+    }
+      else None
+    }
   println(unapply("https://aws.amazon.com/console/home?state=hash&isauthcode=true&code=112"))
+
 }
+
